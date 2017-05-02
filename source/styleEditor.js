290eeea4c9dbc35a2dd92150d47986d76bd110a7
Charlotte Moore <mooc123@gmail.com>
2017-05-02 15:35:38 -0600

    can change stuff on DOM with form

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,15 @@
+
+var doSomething = function(){
+  event.preventDefault();
+  var values = {};
+  var $inputs = $('#style_editor :input' );
+  $inputs.each(function(){
+    values[this.name] = $(this).val();
+  });
+  $(values.selector).css(values.property, values.value);
+};
+
 $(document).ready(function(){
-  // your code goes here.
+  $("#style_editor").submit(doSomething)
+
 });
