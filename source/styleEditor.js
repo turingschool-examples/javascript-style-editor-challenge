03cb6336994dc8c41fdce28803b18527715d3cc1
Ryan Spink <ryanspinktech@gmail.com>
2017-05-02 17:30:17 -0600

    completed challenge

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
+
+function changeContent(){
+  event.preventDefault();
+  var cssSelector = $('#style_editor')[0].elements[0].value;
+  var cssName    = $('#style_editor')[0].elements[1].value;
+  var cssValue       = $('#style_editor')[0].elements[2].value;
+
+  $(cssSelector).css(cssName, cssValue);
+};
+
 $(document).ready(function(){
-  // your code goes here.
+  $('.submit').click(changeContent)
 });
