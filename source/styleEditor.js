4228263d1ca22da87ceb1d0b5ee970800683ba0c
Andy Wooten <andrewdwooten@icloud.com>
2017-05-03 08:09:40 -0600

    Complete challenge
    Adds function that monitors to #style_editor for submission.
    On submission, the values from the form are used to modify
    the desired css propery.

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,15 @@
+
 $(document).ready(function(){
-  // your code goes here.
+
+  $('#style_editor').submit(function(){
+    var selector = $('input[name=selector]').val();
+    var property = $('input[name=property]').val();
+    var value = $('input[name=value]').val();
+    $(selector).css(property, value);
+  });
+
+  $('form').on('submit', function(event){
+    event.preventDefault()
+  });
+
 });
