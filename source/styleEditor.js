9ce660b83974f88d7a25e0ad0aa8f546304459cb
slague <stephanielague@gmail.com>
2017-06-27 16:05:18 -0600

    style editor complete

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  // your code goes here.
+
+
+// 3. binding the previous two parts to a particular event using an event handler.
+// this is a post request...
+
+  $('input[type="submit"]').on('click', function () {
+    event.preventDefault();
+    // 1. retrieving the values from the form,
+    var selector = $('input[name="selector"]').val()
+    var property = $('input[name="property"]').val()
+    var value =    $('input[name="value"]').val()
+
+// 2. passing those values into the correct jQuery function for manipulating CSS
+    $(selector).css(property, value);
+
+  })
+
+
+
 });
