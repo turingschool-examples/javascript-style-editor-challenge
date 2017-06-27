5586e466d75f988123650e38de30c9b1a822e365
lukyans <sukyans@gmail.com>
2017-06-27 16:37:43 -0600

    add style editor

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  // your code goes here.
+ var selector = $('input[name="selector"]');
+ var property = $('input[name="property"]');
+ var value = $('input[name="value"]');
+
+ var submit = $('input[type="submit"]')
+
+  submit.on("click", function(event){
+    event.preventDefault();
+    var selectorValue = selector.val();
+    var propertyValue = property.val();
+    var valueValue = value.val();
+    $(selectorValue).css(propertyValue, valueValue)  
+  });
 });
+
+
