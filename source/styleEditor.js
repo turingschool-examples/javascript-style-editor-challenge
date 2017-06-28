5b6322822be55dd274fc772609c9315b7c001dd3
NessEFC <NessEFC@gmail.com>
2017-06-27 21:14:56 -0600

    initial commit, finished challenge

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-  // your code goes here.
+$(document).ready(function() {
+  $(':submit').on('click', function() {
+    event.preventDefault();
+
+    var form = document.querySelector('form');
+
+    var selector = form.elements[0].value;
+    var prop = form.elements[1].value;
+    var value = form.elements[2].value;
+
+    $(selector).css(prop, value);
+  });
 });
