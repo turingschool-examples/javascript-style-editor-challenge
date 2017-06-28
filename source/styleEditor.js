c0cc9481492fc6bedef5e2a04493a47225e098d6
Josh Thompson <thompsonjoshd@gmail.com>
2017-06-27 22:40:11 -0600

    finish first challenge

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-  // your code goes here.
+$(document).ready(function() {
+
+  const submit = document.querySelector('.container input[type="submit"]')
+
+  function updateCss(e) {
+    const selector = document.querySelector('.container input[name="selector"]').value
+    const property = document.querySelector('.container input[name="property"]').value
+    const value = document.querySelector('.container input[name="value"]').value
+
+    const select = document.querySelector(selector)
+
+    select.style[property] = value
+
+    console.log("tried to set ", select, " to ", property, value);
+
+  }
+
+  // submit.addEventListener('click', updateCss)
+  submit.addEventListener('click', event => {
+    event.preventDefault();
+    updateCss(event);
+  });
+
 });
