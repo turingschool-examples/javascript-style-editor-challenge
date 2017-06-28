6207b7b9e02c3a59b86b19fc716306ffd1712c4f
stovermc <stovermc@gmail.com>
2017-06-27 21:01:19 -0600

    clean up old refactor

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,7 +1,8 @@
 function stylePage() {
-  var cssSelector = $('input[name="selector"]').value
-  var cssProperty = $('input[name="property"]').value
-  var cssValue    = $('input[name="value"]')[0].value
+  event.preventDefault();
+  var cssSelector = $('input[name="selector"]').val()
+  var cssProperty = $('input[name="property"]').val()
+  var cssValue    = $('input[name="value"]').val()
 
   $(cssSelector).css(cssProperty, cssValue)
 }
