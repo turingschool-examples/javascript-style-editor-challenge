119ab7ad0baa8946447c854878ab6482acc40be2
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-15 09:51:17 -0600

    completed challenge

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-  // your code goes here.
+$(document).ready(function() {
+    let selector = $("input[name='selector']")
+    let property = $("input[name='property']")
+    let value = $("input[name='value']")
+    let submit = $("input[type='submit']")
+
+    $(submit).click(function(event) {
+        event.preventDefault();
+        $(selector.val()).css(property.val(), value.val())
+    })
 });
