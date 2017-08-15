3be7a11524d2d258f31c8d286284bf04da7e1568
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-08-15 16:14:12 -0600

    Modify DOM styling from form input

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-  // your code goes here.
+$(document).ready(() => {
+  $('input[type=submit]').on('click', () => {
+    // prevent the default action of submitting the form,
+    // which would otherwise be to send an HTTP request to submit the form
+    event.preventDefault()
+
+    let selector = $('input[name=selector]').val()
+    let property = $('input[name=property]').val()
+    let value = $('input[name=value]').val()
+    updateStyling(selector, property, value)
+  })
+
+  function updateStyling(selector, property, value) {
+    $(selector).css(property, value);
+  }
 });
