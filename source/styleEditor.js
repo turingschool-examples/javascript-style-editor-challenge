338eb32fa1c08daa693b30893e2e89435bc1c5a1
Chris Smith <42@chrisryansmith.com>
2017-08-15 10:05:20 -0600

    add event listener and access form values

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,14 @@
+const processSubmit = (e) => {
+  e.preventDefault();
+
+  const selector = $('#style_editor input[name="selector"]').val()
+  const property = $('#style_editor input[name="property"]').val()
+  const value = $('#style_editor input[name="value"]').val()
+
+  console.log(selector, property, value)
+}
+
+
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit(processSubmit)
 });
