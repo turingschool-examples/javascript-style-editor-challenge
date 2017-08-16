c801bcd9b8ecd16886dfd468fdc656d60c40a87a
Benjamin Ross <benjaminross1001@gmail.com>
2017-08-15 18:44:08 -0600

    can manipulate styling

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('input[type="submit"]').on('click', function() {
+	event.preventDefault();
+  	var selector = $('input[name="selector"]').val();
+  	var property = $('input[name="property"]').val();
+  	var value = $('input[name="value"]').val();
+
+  	$(selector).css(property, value);
+  })
 });
