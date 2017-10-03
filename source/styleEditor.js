a1de0644bc2738d2040e475f15a0e649a25e3276
Wil Donovan <wil.donovan@gmail.com>
2017-10-03 17:53:06 -0600

    style editor finished. eventprevent default is quirky.  Might be js version? or atom

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // your code goes here.
+
+  $("#style_editor").on("submit", function(event) {
+    event.preventDefault() if event.preventDefault;
+      var $selector = $("#selector").val();
+      var $property = $("#property").val();
+      var $value = $("#value").val();
+      console.log($value)
+      console.log($property)
+      console.log($selector)
+      $($selector).css($property, $value);
+  });
 });
