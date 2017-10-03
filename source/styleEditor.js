ed3b8e45444f27ee5752368d6db7bdc8a7f3467a
Chantal Justamond <cjustamm66@me.com>
2017-10-03 12:42:09 -0600

    css displaying in browser accordingly

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-  // your code goes here.
+$(document).ready(function () {
+    let selector = $('input[name="selector"]');
+    let property = $('input[name="property"]');
+    let value = $('input[name="value"]');
+
+    // ES5
+    // $('input[type="submit"]').on('click', function (event) {
+    //     event.preventDefault();
+    //     selector = selector.val();
+    //     $(selector).css(property.val(), value.val())
+    //
+    // });
+
+    // ES6 getting used to fat arrows
+    $('input[type="submit"]').on('click', (event) => {
+        event.preventDefault();
+         selector = selector.val();
+        $(selector).css(property.val(), value.val())
+    })
 });
