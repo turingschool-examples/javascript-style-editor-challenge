6d643d95e1d79bb09aedcd3769d502326ff6e35a
wratterman <will.ratterman@gmail.com>
2017-10-03 11:20:34 -0600

    added basic structure I envision for the desired action

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   // your code goes here.
+  $("#style_editor").on("submit", function(){
+    let selector = $(//#parsed through #selector).val();
+    let property = $("#property[]"//#parsed through #property).val();
+    let value = $(//#parsed through #value).val();
+    $(selector).css(property, value);
+  })
 });
