6766d42486cc0e2195be384edc0c4b0650a0c1c7
JF-Lalonde <lalondej88@gmail.com>
2017-10-03 12:22:02 -0600

    Refactor to eliminate map prototype function

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
   $('#style_editor').on('submit', function(e){
     e.preventDefault();
     var data = $("#style_editor :input").serializeArray();
-    var results = data.map(function(d){
-      return d.value
-    })
-    $(results[0]).css(results[1], results[2])
+    $(data[0].value).css(data[1].value, data[2].value)
   });
 });
