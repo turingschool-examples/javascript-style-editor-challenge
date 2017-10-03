710b30a4a0f3427d26010f4510e55d2fdd4be28b
wratterman <will.ratterman@gmail.com>
2017-10-03 12:48:23 -0600

    got the challenge working

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
   $("#style_editor").on("submit", function(){
     let selector = $("[name=selector]").val();
-    alert(selector);
-    let property = //#parsed through #property).val();
-    let value = $(//#parsed through #value).val();
+    let property = $("[name=property]").val();
+    let value = $("[name=value]").val();
     $(selector).css(property, value);
     event.preventDefault()
   })
