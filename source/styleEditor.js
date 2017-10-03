b6bff05226146d6644768c2e06b57f0c97ff5450
JF-Lalonde <lalondej88@gmail.com>
2017-10-03 15:58:45 -0600

    Refactored to use 'this ' to capture form data

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#style_editor').on('submit', function(e){
     e.preventDefault();
-    var $data = $("#style_editor :input").serializeArray();
+    var $data = $(this).serializeArray();
     $($data[0].value).css($data[1].value, $data[2].value)
   });
 });
