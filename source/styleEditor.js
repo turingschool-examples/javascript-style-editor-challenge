eb673e6d2f498748c0fb631efd8bf8912992b97a
JF-Lalonde <lalondej88@gmail.com>
2017-10-03 12:32:47 -0600

    Change data variable to  for jQuery variable

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#style_editor').on('submit', function(e){
     e.preventDefault();
-    var data = $("#style_editor :input").serializeArray();
-    $(data[0].value).css(data[1].value, data[2].value)
+    var $data = $("#style_editor :input").serializeArray();
+    $($data[0].value).css($data[1].value, $data[2].value)
   });
 });
