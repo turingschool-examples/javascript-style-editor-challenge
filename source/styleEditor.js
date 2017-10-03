998580a3c379943f3586f774e2a382f773aaadad
BFunk <bret.e.funk@gmail.com>
2017-10-03 11:37:57 -0600

    first commit

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  // your code goes here.
+  //let selector = ($('selector').attr('name')).val();
+  $('selector').attr('name').submit(alert("test"));
+  //let selector = $("[name='selector']").val();
+  //let selector = $("[name='selector']").val();
+  //let property = $("[name='property']").val();
+  //let value = $("[name='value']").val();
 });
