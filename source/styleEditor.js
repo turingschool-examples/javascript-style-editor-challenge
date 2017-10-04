e3b313a06aed903cb20d875a5b662302427c7578
Wil Donovan <wil.donovan@gmail.com>
2017-10-03 18:39:55 -0600

    fixed the prevent default error... ; ; was the problem

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
   $("#style_editor").on("submit", function(event) {
-    event.preventDefault() if event.preventDefault;
-      var $selector = $("#selector").val();
-      var $property = $("#property").val();
-      var $value = $("#value").val();
-      console.log($value)
-      console.log($property)
-      console.log($selector)
-      $($selector).css($property, $value);
+    event.preventDefault();
+    var $selector = $("#selector").val();
+    var $property = $("#property").val();
+    var $value = $("#value").val();
+    console.log($value);
+    console.log($property);
+    console.log($selector);
+    $($selector).css($property, $value);
   });
 });
