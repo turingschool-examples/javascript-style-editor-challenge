bb0d7e0e58ee4298b03f2223f06c3215141c0595
MasonHolland <masontholland@gmail.com>
2017-10-03 19:19:15 -0600

    finish styleEditor

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  // your code goes here.
+
+  function changeStyle(selector, property, value) {
+    $(selector).css(property, value);
+  }
+
+  $('#style_editor').submit(function() {
+    event.preventDefault();
+    let selector = $('#style_editor :input')[0].value
+    let property = $('#style_editor :input')[1].value
+    let value    = $('#style_editor :input')[2].value
+
+    changeStyle(selector, property, value);
+  })
 });
