f49d3ec30ff7aae58c5af101c3fb7ef1a8d522d5
sidewinder2020 <sdwinder1@gmail.com>
2017-11-28 20:41:51 -0700

    It does the thing

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // your code goes here.
+  $( "#submit_button_thingy" ).on(
+    "click",
+    function( eventObject ) {
+      eventObject.preventDefault();
+      var cssSelectorThingy = $( "#css_select_thingy" ).val();
+      var propertyThingy = $( "#property_thingy" ).val();
+      var valueThingy = $( "#value_thingy" ).val();
+
+      $( cssSelectorThingy ).css( propertyThingy, valueThingy );
+    }
+);
 });
