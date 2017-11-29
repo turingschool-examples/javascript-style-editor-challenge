72ab477200c7afd3dd7959b08b34679d70b1417c
Yohanan Assefa <nanahoy@gmail.com>
2017-11-28 18:20:00 -0700

    Add jquery to styleEditor.js

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   // your code goes here.
+
+  $("#style_editor").submit(function() {
+    var selector = $("input[name='selector']" ).val();
+    var property = $("input[name='property']" ).val();
+    var value = $("input[name='value']" ).val();
+    $(`${selector}`).css(`${property}`, `${value}`);
+    event.preventDefault();
+  });
 });
