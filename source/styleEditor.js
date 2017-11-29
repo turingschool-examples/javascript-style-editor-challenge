487d36c619f73b59c9bc907507ecb877b80f2447
Aaron Hursh <amhursh@gmail.com>
2017-11-28 23:32:07 -0700

    Complete challenge

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit(function(event) {
+    event.preventDefault();
+
+    var cssSelector = $('input[name=selector]').val();
+    var property = $('input[name=property]').val();
+    var value = $('input[name=value]').val();
+
+    $(cssSelector).css(property, value);
+  })
 });
