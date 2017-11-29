2ffac74287e3325c33436e5324122c05cb04e11d
Ricky Amparo <rickyamparo@gmail.com>
2017-11-28 17:46:00 -0700

    add missing semicolon

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var selector = $("input[name=selector]").val();
     var property = $("input[name=property]").val();
     var value = $("input[name=value]").val();
-    $(selector).css(property, value)
+    $(selector).css(property, value);
     event.preventDefault();
   });
 
