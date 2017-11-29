309d0d26e6726b04674e3f1919d64a448a6da596
Aram Anderson <aram.j.anderson@gmail.com>
2017-11-28 20:33:05 -0700

    done

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit(function(event) {
+    var selector = $('input[name=selector').val();
+    var property = $('input[name=property').val();
+    var value = $('input[name=value').val();
+
+    $(selector).css(property, value);
+    event.preventDefault();
+  })
 });
