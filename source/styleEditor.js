a8b9625286a30a1c514c42d1ca6be3ef7accf0aa
jayphodges <jayphodges@gmail.com>
2017-11-28 19:44:29 -0700

    Functionally working in theory

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-  $("#style_editor").on("submit", function (event) {
+  $("#style_editor").on("submit", event => {
 
     var selector = $('[name=selector]').val();
     var property = $('[name=property]').val();
     var value = $('[name=value]').val();
-    console.log(selector);
-    console.log(property);
-    console.log(value);
+
     $(selector).css(property, value);
     event.preventDefault();
   })
