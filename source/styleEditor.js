dfbc81a452a655b1b2823908bbd5898ceace7e1d
Adrian Lara <adrianblara@gmail.com>
2018-01-23 14:37:09 -0700

    Adrian Lara - 1708BE

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  // your code goes here.
+  $(".container [type=submit]").on("click", function(event) {
+    event.preventDefault();
+
+    let field = $(".container #style_editor [name=selector]").val()
+    let property = $(".container #style_editor [name=property]").val()
+    let value = $(".container #style_editor [name=value]").val()
+
+    styleIt(field, property, value)
+  })
 });
+
+function styleIt(field, property, value) {
+  $(field).css(property, value)
+}
