f71425dc598c273f2f98a9f6a077721d97ab8b6a
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-23 12:36:22 -0700

    completed styling

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').on("submit", function() {
+    event.preventDefault();
+    var element = $('[name=selector]').val();
+    var property = $('[name=property]').val();
+    var value = $('[name=value]').val();
+
+    console.log($(`${element}`).css(`${property}`, `${value}`).val())
+  })
 });
