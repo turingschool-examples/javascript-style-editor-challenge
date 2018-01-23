4512d43b426ad81ffe0c0d1b41d4fb7b083bca2d
Marshall Houston <marshall.houston@gmail.com>
2018-01-23 15:26:13 -0700

    remove unnecessary quotes to selectors

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('input[type="submit"]').click( function() {
+  $('input[type=submit]').click( function() {
     event.preventDefault();
-    var selector = $('input[name="selector"]').val();
-    var property = $('input[name="property"]').val();
-    var value = $('input[name="value"]').val();
+    var selector = $('input[name=selector]').val();
+    var property = $('input[name=property]').val();
+    var value = $('input[name=value]').val();
     changeStyle(selector, property, value);
   })
 });
