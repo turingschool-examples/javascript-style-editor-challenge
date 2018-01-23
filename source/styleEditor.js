cb1906415be3077b28358fb61618339e71632bb8
Marshall Houston <marshall.houston@gmail.com>
2018-01-23 15:18:34 -0700

    add function to change the styling of particular element

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('input[type="submit"]').click( function() {
+    event.preventDefault();
+    var selector = $('input[name="selector"]').val();
+    var property = $('input[name="property"]').val();
+    var value = $('input[name="value"]').val();
+    changeStyle(selector, property, value);
+  })
 });
+
+function changeStyle(selector, property, value) {
+  $(`${selector}`).css(`${property}`, `${value}`);
+}
