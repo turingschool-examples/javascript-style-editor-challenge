be49e1d6c7a4fc6397b0b5bc4fe4200bf2a181d1
izaxveta <izavetha@gmail.com>
2018-01-23 15:34:03 -0700

    Added stylePage functionality using input element values, preventDefault, location.reload() for empty inputs

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  // your code goes here.
-});
+  $('form').on('submit', function(event) {
+    event.preventDefault()
+  })
+  var inputElements = document.getElementsByTagName('input')
+  var submitButton  = inputElements[3]
+  submitButton.addEventListener('click', stylePage)
+})
+
+function stylePage() {
+  var selector = $('input[name=selector]').val()
+  var property = $('input[name=property]').val()
+  var value = $('input[name=value]').val()
+  if(selector == '' || property == '' || value == '') {
+    location.reload()
+  } else {
+    $(selector).css(property, value)
+  }
+}
