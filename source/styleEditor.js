afe4b00a512b34ee479ebb8af252258365ee516b
Dione Wilson <dionew1@gmail.com>
2018-01-23 19:25:05 -0700

    Fix linting errors

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $("input[type=submit]").click(function() {
-    event.preventDefault();
-    var cssSelector = $("input[name=selector]").val();
-    var cssProperty = $("input[name=property]").val();
-    var cssValue = $("input[name=value]").val();
-    $(cssSelector).css( cssProperty, cssValue);
-  });
+	$("input[type=submit]").click(function() {
+		event.preventDefault();
+		var cssSelector = $("input[name=selector]").val();
+		var cssProperty = $("input[name=property]").val();
+		var cssValue = $("input[name=value]").val();
+		$(cssSelector).css( cssProperty, cssValue);
+	});
 });
