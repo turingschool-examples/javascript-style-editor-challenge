4f855077e386a9f84325fe1d33706257e12aade5
Emily Wise <wise.m.emily@gmail.com>
2018-01-24 15:04:08 -0700

    updated the css

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-  // your code goes here.
-});
+
+
+$("form").submit(styleCss);
+
+function styleCss(event) {
+  event.preventDefault();
+  var css_selector  = $('#selector').val();
+  var property_input = $('#property').val();
+  var value_input = $('#value').val();
+$(css_selector).css(property_input, value_input);
+};
