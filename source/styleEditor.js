c0d535b0d9eb6732064a45155aa8c55cd44702a3
BLaurenB <blbillington1@gmail.com>
2018-03-10 21:18:13 -0700

    Update styleEditor.js

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -11,9 +11,3 @@ $(document).ready(function(){
 
 // file:///Users/laurenbillington/turing/4mod/prework/javascript-style-editor-challenge/source/index.html?selector=h1&property=color&value=blue
 
-
-// $('a').click(function (event) {
-//   event.preventDefault();
-//   $('<div>').append('default ' + event.type + ' prevented').appendTo('#log')
-// })
-
