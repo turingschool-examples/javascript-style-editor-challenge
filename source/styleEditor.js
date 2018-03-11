e704dc984aef6f10cee3d2758f9cc6a6e6c19e76
BLaurenB <blbillington1@gmail.com>
2018-03-10 20:48:53 -0700

    doing stuff

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
   // your code goes here.
 });
-$("input[style=submit]").on("click", function () {
-  console.log($(this).val());
+$("input[style=submit]").on("click", function (event) {
+  event.preventDefault();
+  console.log($('input[name=selector]'));
+
 });
+
+// file:///Users/laurenbillington/turing/4mod/prework/javascript-style-editor-challenge/source/index.html?selector=h1&property=color&value=blue
+
 
 // $('a').click(function (event) {
 //   event.preventDefault();
