b6b9a8a27d89ce215090121838d1d6d5679019b6
BLaurenB <blbillington1@gmail.com>
2018-03-10 20:38:36 -0700

    trying to figure things out

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   // your code goes here.
 });
+$("input[style=submit]").on("click", function () {
+  console.log($(this).val());
+});
+
+// $('a').click(function (event) {
+//   event.preventDefault();
+//   $('<div>').append('default ' + event.type + ' prevented').appendTo('#log')
+// })
+
